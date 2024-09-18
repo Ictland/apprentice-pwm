b9e5422db995cd051d14d8a9f7b1b701d6c6fe6c
Ictland <86009099+Ictland@users.noreply.github.com>
2024-09-18 22:48:01 +0900

    Add some change pw manager script

--- password_manager.sh
+++ password_manager.sh
@@ -6,6 +6,9 @@ echo "パスワードマネージャーへようこそ！"
 echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
 read choice
 
+
+case $choice in
+"Add Password")
 echo "サービス名を入力してください："
 read service_name
 echo "ユーザー名を入力してください："
@@ -16,3 +19,5 @@ read password
 echo "$service_name:$username:$password" >> "$PW_FILE"
 
 echo "パスワードの追加は成功しました。"
+;;
+"Get Password")
