6e5657b485022981712b2567866cc9b5d39c53c1
Ictland <86009099+Ictland@users.noreply.github.com>
2024-09-18 16:08:49 +0900

    add some changes to pwm

--- password_manager.sh
+++ password_manager.sh
@@ -1,4 +1,10 @@
 
 PW_FILE="passwords.txt"
 
-echo "パスワードマネージャーへようこそ！"
+echo "パスワードマネージャーへようこそ！
+echo "サービス名を入力してください："
+service_name
+echo "ユーザー名を入力してください："
+username
+echo "パスワードを入力してください："
+password
