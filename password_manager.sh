d6e30523a8ee18337280488bb0a8cb3cdf840890
Ictland <86009099+Ictland@users.noreply.github.com>
2024-09-18 21:28:02 +0900

    Add some pw manager script

--- password_manager.sh
+++ password_manager.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 
 PW_FILE="passwords.txt"
 
-echo "パスワードマネージャーへようこそ！
+echo "パスワードマネージャーへようこそ！"
+
 echo "サービス名を入力してください："
 read service_name
 echo "ユーザー名を入力してください："
@@ -9,4 +11,6 @@ read username
 echo "パスワードを入力してください："
 read password
 
-echo "$service_name:$username:$password" >> passwords.txt
+echo "$service_name:$username:$password" >> "$PW_FILE"
+
+echo "パスワードの追加は成功しました。"
